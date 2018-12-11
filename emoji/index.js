3fb4d68f0c0f3d6fc163308bfd96e50d638c3fc2
Erwin Graanstra <kurdt94@gmail.com>
2018-12-12 00:23:52 +0100

    Add files via upload

---
+++ emoji/index.js
@@ -0,0 +1,7 @@
+// Exports the "emoji" plugin for usage with module loaders
+// Usage:
+//   CommonJS:
+//     require('tinymce/plugins/emoji')
+//   ES2015:
+//     import 'tinymce/plugins/emoji'
+require('./plugin.js');
